345733acb3a729581180b04962158efe6e28a133
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-03-06 23:29:07 +0300

    editted

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+let computedName= ""
 function onSubmit(e) {
-   // e.preventDefault();
+    e.preventDefault();
     const maleNames = ['Kwasi',' Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
     const femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
     const answer= document.getElementById('dateOfBirth');
@@ -7,15 +8,15 @@ function onSubmit(e) {
     console.log(selectedDate.getFullYear());
     const yy =parseInt(selectedDate.getFullYear().toString().slice(0,1));
     const cc= parseInt(selectedDate.getFullYear().toString().slice(1,2))
-    const mm = parseInt(selectedDate.getMonth().value);
-    const dd = parseInt(selectedDate.getDate().value);
+    const mm = parseInt(selectedDate.getMonth());
+    const dd = parseInt(selectedDate.getDate());
     console.log(yy,cc,mm,dd)
     const male = document.getElementById('male').value;
     const female = document.getElementById('female').value;
-    const button = document.getElementById('btn').value;
+    console.log(male,female)
 
-    const day= (((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
-    switch(day) {
+    const day= (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+    switch(Math.floor(day)) {
         case 0:
             dayName= 'Sunday'
             break;
@@ -41,44 +42,63 @@ function onSubmit(e) {
         default: dayName= 'invalid Day'
             break;
     }
-    console.log(dayName)
+    console.log(Math.floor(day));
 
     function results() {
-        if(day===0 && maleNames[0]) {
-            console.log(`Your name is ${maleNames[0]}`)
-        } else if (day===0 && femaleNames[0]){
+        if(day===0 && gender == "male") {
+            console.log(`Your name is ${maleNames[0]}`);
+             computedName= maleNames[0];
+        } else if (day===0 &&  gender == "female"{
             console.log(`Your name is ${femaleNames[0]}`)
-        } else if (day===1 && maleNames[1]){
+            computedName= femaleNames[0];
+        } else if (day===1 &&  gender == "male"){
             console.log(`Your name is ${maleNames[1]}`)
-        } else if (day===1 && femaleNames[1]){
+            computedName= maleNames[1];
+        } else if (day===1 && gender == "female"{
             console.log(`Your name is ${femaleNames[1]}`)
-        } else if (day===2 && maleNames[2]){
+            computedName= femaleNames[1];
+        } else if (day===2 && gender == "male"){
             console.log(`Your name is ${maleNames[2]}`)
-        } else if (day===2 && femaleNames[2]){
+            computedName= maleNames[2];
+        } else if (day===2 && gender == "female"){
             console.log(`Your name is ${femaleNames[2]}`)
-        } else if (day===3 && maleNames[3]){
+            computedName= femaleNames[2];
+        } else if (day===3 && gender == "male"){
             console.log(`Your name is ${maleNames[3]}`)
-        } else if (day===3 && femaleNames[3]){
-            console.log(`Your name is ${femaleNames[3]}`);    
-        }  else if (day===4 && maleNames[4]){
+            computedName= maleNames[3];
+        } else if (day===3 && gender == "female"){
+            console.log(`Your name is ${femaleNames[3]}`);   
+            computedName= maleNames[3];
+        }  else if (day===4 && gender == "male"){
             console.log(`Your name is ${maleNames[4]}`);  
-        }  else if (day===4 && femaleNames[4]){
+            computedName= maleNames[4];
+        }  else if (day===4 && gender == "female"){
             console.log(`Your name is ${femaleNames[4]}`); 
-        }  else if (day===5 && maleNames[5]){
+            computedName= maleNames[4];
+        }  else if (day===5 && gender == "male"){
             console.log(`Your name is ${maleNames[5]}`);
-        }  else if (day===5 && femaleNames[5]){
-            console.log(`Your name is ${femaleNames[5]}`);  
-        }  else if (day===6 && maleNames[6]){
-            console.log(`Your name is ${maleNames[6]}`);   
-        }  else if (day===6 && femaleNames[6]){
-            console.log(`Your name is ${femaleNames[6]}`);       
+            computedName= maleNames[5];
+        }  else if (day===5 && gender == "female"){
+            console.log(`Your name is ${femaleNames[5]}`); 
+            computedName= femaleNames[5]; 
+        }  else if (day===6 && gender == "male"){
+            console.log(`Your name is ${maleNames[6]}`); 
+            computedName= maleNames[6];  
+        }  else if (day===6 && gender == "female"){
+            console.log(`Your name is ${femaleNames[6]}`); 
+            computedName= femaleNames[6] 
 
     }}
+    }
     results()
 }
+
 document.getElementById('btn').addEventListener('click', onSubmit);
-let result = document.getElementById("reveal-result").innerHTML = "Your akan name is " + onSubmit() + "!";
+if(computedName != ""){
+    document.getElementById("reveal-result").innerHTML = "Your akan name is " + computedName + "!"; 
+} 
 document.getElementById("myForm").reset();
+
 
 
 
